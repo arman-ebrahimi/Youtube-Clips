869f9c7166936bcfc68f9fc7214c8324be1b0364
Arman Ebrahimi <ebrahimi.arman72@gmail.com>
2023-03-01 18:13:11 -0800

    adding related videos

--- src/components/videoDetail.js
+++ src/components/videoDetail.js
@@ -1,18 +1,36 @@
 import {useLocation} from "react-router-dom";
 import ReactPlayer from "react-player/youtube";
+import {useEffect, useState} from "react";
+import {fetchFromApi} from "../api/fetchFromApi";
+import {Link} from "react-router-dom";
 
 export const VideoDetail = () => {
     const videoDetail = useLocation().state;
     const {id: {videoId}, snippet: {title, description, channelId, channelTitle}} = videoDetail;
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        fetchFromApi(`search?part=snippet&relatedToVideoId=${videoId}&type=video`)
+            .then((data) => setVideos(data.items))
+    }, [])
     return(
         <div className="video-detail">
             <div className="single-video-box text-white">
                 <ReactPlayer url={`https://www.youtube.com/watch?v=${videoId}`} controls />
                 <p className="mt-5">{title}</p>
                 <p>{description}</p>
-                <div>Channel: <a href={`https://www.youtube.com/channel/${channelId}`}>{channelTitle}</a></div>
+                <div>Visit Channel <span className="fa fa-angle-double-right"></span><span className="fa fa-angle-double-right"></span>
+                    {" "}<a href={`https://www.youtube.com/channel/${channelId}`}>{channelTitle}</a>
+                </div>
             </div>
             <div className="related-videos-box">
+                {videos.map((item, index) => {
+                    return(
+                        <Link key={index} to="/videoDetail" state={item}>
+                            <img src={item?.snippet?.thumbnails?.medium?.url} />
+                        </Link>
+                    )
+                })}
             </div>
         </div>
     )
--- src/data/data.js
+++ src/data/data.js
@@ -5,7 +5,7 @@ export const allVideos = {
     English_Podcasts: ["ZjIRYn7x8sk", "lhFU5H5KPFE", "IB8JiCnk09s", "d-mL1MnkCD0", "JLaeBWgodhA", "WhM5DwavnMA", "hCjPjoolzMs", "XdmpZdRYF3I", "iLlrIi9-NfQ"],
     game: ["JwMSjCRNIfk", "QaFKukn0a6g", "opyIAIQvYzU", "NWjbDq-o5pA", "0QYefGgHO9o", "e3tI78Vy0Xs", "-mIIzGgYsG0", "2RdH8tev6tQ", "ehQ-E9vt4IE"],
     sports: ["qE28c_NDgHM", "CnucTZ0XAI8", "oQ4_yZvdiz8", "vSBcrmx4aFw", "8F8bwlPQEZc", "vArU3nn-21I", "CXLM08fZO5o", "kuSZEZ0fCY0", "wgZZhHOZJJQ"],
-    reactjs: ["bMknfKXIFA8", "Tn6-PIqc4UM", "Ke90Tje7VS0", "iZhV0bILFb0", "ly3m6mv5qvg", "RVFAyFWO4go", "Reny0cTTv24", "SK9AlIbexOE", "K9bYxUREypk"],
+    reactjs: ["bMknfKXIFA8", "KjY94sAKLlw", "Tn6-PIqc4UM", "Ke90Tje7VS0", "iZhV0bILFb0", "ly3m6mv5qvg", "RVFAyFWO4go", "Reny0cTTv24", "SK9AlIbexOE", "K9bYxUREypk"],
     traveling: ["RSF8KL3xaIk", "cOxBMWBS9U8", "1ME0hmW6SLM", "t7aoiYeH4mc", "BwOqK5hYFKo", "IlU6l_d3ff8", "KtdfW8tOiSc", "wroGPb4-3yM", "xXwJoweRtxI"],
     cars: ["K9W0MtwrK98", "gOK0GEo3m4E", "01Ic9Hb2cRs", "mWMUWO13wN4", "zOsdfk0P4Ig", "bJZCM0lke_0", "uZZd2laEPJo", "tlThdr3O5Qo", "3jDO5NT12AI"],
     cooking: ["Dqstaunpae0", "z0aLnBT9fWI", "8G-ZxTjD7Zc", "Ak34skdOeNI", "nOpV4UnPebo", "OhhqXDKDYOk", "yPiS5zB1tDo", "rD0VO_ETYSg", "ulhRORJpuBM"],
