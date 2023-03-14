2bcc344a7cf2d84cf2cd11badef10bf974afe5b7
unknown <ebrahimi.arman72@gmail.com>
2023-03-14 14:16:16 +0330

    fixing an error

--- src/components/searchResult.js
+++ src/components/searchResult.js
@@ -1,6 +1,5 @@
 import {useSelector} from "react-redux";
 import {Videos} from "./videos";
-import {useEffect, useState} from "react";
 
 export const SearchResult = () => {
     const videos = useSelector(state => state.search);
